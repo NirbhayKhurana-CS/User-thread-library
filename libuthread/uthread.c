4fe14c8c7ccf9f47e972d001ba68ab5f2cc541fd
jackieden26 <jackiedeng26@gmail.com>
2019-02-03 14:13:11 -0800

    Change threadControl to global static variable

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -14,23 +14,32 @@
 /* TODO Phase 2 */
 #define MAX_THREAD 32768
 
-enum state{running, ready, blocked, zombie};
+enum status{running, ready, blocked, zombie};
 
+// Forward decleration because two struct includes each other's pointer.
 typedef struct Thread Thread;
 typedef struct ThreadControl ThreadControl;
 
 struct Thread {
     uthread_t threadId;
+    int state;
     void *stack;
-    int stateId;
     uthread_ctx_t *context;
     ThreadControl *controlAddr;
 };
 
 struct ThreadControl {
-    int threadCount; // Thread count doesn't include the main thread
+    int threadCount; // ThreadCount doesn't include the main thread
     Thread *threadArray[MAX_THREAD];
 };
+
+static ThreadControl *threadControl;
+
+/*
+ * count is the number of totoal threads, including main threads.
+ * It is static to this file.
+ */
+static int count = 0;
 
 void uthread_yield(void)
 {
@@ -50,28 +59,25 @@ int first_uthread_create(ThreadControl *threadControl) {
     }
     firstThread->stack = uthread_ctx_alloc_stack();
     firstThread->threadId = 0;
-    // TODO statusID
+    // TODO state
     threadControl->threadArray[0] = firstThread;
     return 0;
 }
 
 int uthread_create(uthread_func_t func, void *arg) {
     // This is the new thread to register.
-    static ThreadControl *threadControl;
     Thread *thread = malloc(sizeof(Thread));
     if (thread == NULL) {
         return -1;
     }
-    static int count = 0;
     if (count == 0) {
-        ThreadControl *threadControl = malloc(sizeof(ThreadControl));
+        // ThreadControl *threadControl = malloc(sizeof(ThreadControl));
         if (threadControl == NULL) {
             return -1;
         }
         if (first_uthread_create(threadControl) == -1) {
             return -1;
         }
-
     }
     count++;
     thread->stack = uthread_ctx_alloc_stack();
